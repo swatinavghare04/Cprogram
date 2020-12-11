a4a739deb83785cb59da1f2f887b8e57b4a0d25f
swatinavghare04 <75429629+swatinavghare04@users.noreply.github.com>
2020-12-11 14:02:25 +0530

    C program - Division of two numbers

---
+++ DIVISION.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+
+{
+int a,b;
+int div;
+clrscr();
+printf("enter two  umbers :");
+scanf("%d%d",&a,&b);
+div=a/b;
+printf("Division of two numbers is : %d",div);
+getch();
+}
