d62a1688214a12182cded46e96265816ebc77022
swatinavghare04 <75429629+swatinavghare04@users.noreply.github.com>
2020-12-11 14:07:29 +0530

    C program - multiplication of two numbers

---
+++ MULTIPLI.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int num1,num2;
+int mul;
+clrscr();
+printf("enter two numbers : ");
+scanf("num1=%d,num2=%d",&num1,&num2);
+mul=num1*num2;
+printf("multiplication of two numbers is : %d",mul);
+getch();
+}
